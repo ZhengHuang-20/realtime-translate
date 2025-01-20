cf785e8c15cdd904e4783a5729b9f6909bb60c2e
hz <285411845@qq.com>
2025-01-20 14:16:52 +0800

    保存到tmp

--- app/api/text-to-speech/route.ts
+++ app/api/text-to-speech/route.ts
@@ -8,24 +8,6 @@ import fs from 'fs/promises';
 
 const GOOGLE_AI_KEY = process.env.GOOGLE_AI_KEY;
 
-const deleteOldFiles = async (dir: string, maxAge: number) => {
-  try {
-    const files = await fs.readdir(dir);
-    for (const file of files) {
-      const filePath = path.join(dir, file);
-      const stat = await fs.stat(filePath);
-      const fileAge = Date.now() - stat.mtimeMs;
-      if (fileAge > maxAge) {
-        await fs.unlink(filePath);
-      }
-    }
-  } catch (error) {
-    console.error('Error deleting old files:', error);
-  }
-};
-
-
-
 if (!GOOGLE_AI_KEY) {
   throw new Error('GOOGLE_AI_KEY environment variable is not set.');
 }
@@ -34,15 +16,13 @@ const auth = new GoogleAuth({
     apiKey: GOOGLE_AI_KEY,
 });
 
-
 const client = new TextToSpeechClient({
     auth,
 });
 
 export async function POST(req: NextRequest) {
   try {
-    const audioDir = path.join(process.cwd(), 'public', 'audio');
-    await deleteOldFiles(audioDir, 60000);
+    const audioDir = '/tmp';
     const { text, language } = await req.json();
 
     let voice;
@@ -66,18 +46,12 @@ export async function POST(req: NextRequest) {
     if (!response.audioContent) {
       return NextResponse.json({ error: 'Invalid audio content' }, { status: 400 });
     }
-    const filename = `${uuidv4()}.mp3`;
-    try {
-      await fs.access(audioDir);
-    } catch (error) {
-      await fs.mkdir(audioDir, { recursive: true });
-    }
-    const filePath = path.join(audioDir, filename);
-    await fs.writeFile(filePath, Buffer.from(response.audioContent));
 
-    const audioUrl = `/audio/${filename}`;
-
-    return NextResponse.json({ audioUrl });
+    return new NextResponse(response.audioContent, {
+      headers: {
+        'Content-Type': 'audio/mpeg',
+      },
+    });
   } catch (error) {
     console.error('Error synthesizing speech:', error);
     return NextResponse.json({ error: 'Failed to synthesize speech' }, { status: 500 });
